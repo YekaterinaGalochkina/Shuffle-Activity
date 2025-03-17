492029d52ecd8b3bb207197536c801eecdc5cea2
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-03-17 14:50:30 -0700

    Implementing shuffle_deck function

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 
 # Implement a solution for shuffling a deck of cards
 # Use only randint() and no other imports
-def shuffle_deck(deck_of_cards):
-    pass
+def shuffle_deck(deck):
+    for i in range(len(deck_of_cards)):
+        switch_index = randint(0,len(deck_of_cards)-1)
+        temp = deck_of_cards[i]
+        deck_of_cards[i] = deck_of_cards[switch_index]
+        deck_of_cards[switch_index] = temp
+    return deck_of_cards
+
+print(shuffle_deck(deck_of_cards))

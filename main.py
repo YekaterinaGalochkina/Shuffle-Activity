54469c8ec9cb8e7f918e0afca39ae0e540c9e8b4
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-03-17 17:19:29 -0700

    Better solution

--- main.py
+++ main.py
@@ -4,15 +4,34 @@ CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♥", "♦", "♠", "♣"]
 
 deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
+print(deck_of_cards)
 
-# Implement a solution for shuffling a deck of cards
-# Use only randint() and no other imports
-def shuffle_deck(deck):
-    for i in range(len(deck_of_cards)):
-        switch_index = randint(0,len(deck_of_cards)-1)
-        temp = deck_of_cards[i]
-        deck_of_cards[i] = deck_of_cards[switch_index]
-        deck_of_cards[switch_index] = temp
+def shuffle_deck(deck_of_cards):  
+    length = len(deck_of_cards)
+
+    for _ in range(length):
+        random_index = randint(0, length - 1)
+        card = deck_of_cards.pop(random_index)
+        deck_of_cards.append(card)
     return deck_of_cards
 
+print(deck_of_cards)
 print(shuffle_deck(deck_of_cards))
+
+
+
+
+
+
+
+
+
+
+#     for i in range(len(deck_of_cards)):
+#         switch_index = randint(0,len(deck_of_cards)-1)
+#         temp = deck_of_cards[i]
+#         deck_of_cards[i] = deck_of_cards[switch_index]
+#         deck_of_cards[switch_index] = temp
+#     return deck_of_cards
+
+# print(shuffle_deck(deck_of_cards))
